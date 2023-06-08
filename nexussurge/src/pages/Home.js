e785121d30329a9023eae70993a86d17ff881c03
Alexander Popescu <alexanderpopescu5381@gmail.com>
2023-06-07 21:21:52 -0400

    added homepage left side with styling

--- nexussurge/src/pages/Home.js
+++ nexussurge/src/pages/Home.js
@@ -1,99 +1,102 @@
 import Header from '../components/Header.js'
-import { Container, Typography, Stack, Button } from '@mui/material'
-import { Card, CardContent, CardActions, Grid } from '@mui/material'
+import { Container, Typography, Stack, Button, Drawer } from '@mui/material'
+import { Card, CardContent, CardActions, Divider } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
 
-
 export function Home() {
+    /* Example API result from my sets */
+    const test_user_sets_result = [
+        {
+            title: "Test Set 1 - Personal",
+            description: "This is a test set",
+            creator: "Test User 1",
+            num_cards: 10,
+            id: 1
+        },
+        {
+            title: "Test Set 2 - Personal",
+            description: "This is a test set",
+            creator: "Test User 2",
+            num_cards: 10,
+            id: 2
+        },
+        {
+            title: "Test Set 3 - Personal",
+            description: "This is a test set",
+            creator: "Test User 3",
+            num_cards: 10,
+            id: 3
+        },
+        {
+            title: "Test Set 4 - Personal",
+            description: "This is a test set",
+            creator: "Test User 4",
+            num_cards: 10,
+            id: 4
+        },
+        {
+            title: "Test Set 5 - Personal",
+            description: "This is a test set",
+            creator: "Test User 5",
+            num_cards: 10,
+            id: 5
+        }]
+
     return (
         <>
+            <Box sx={{ flexGrow: 1 }}>
             <Header />
-        <div className="body-background">
-        <Box sx={{mt: 0}} className="body-content">
-
-            <h3></h3>
-            <Container fixed >
-            <Typography
-              component="h1"
-              variant="h1"
-              align="center"
-              color="text.primary"
-              gutterBottom
-              fontFamily={'jetbrains mono'}
+            <Box sx={{ display: 'flex' }}>
+            <Drawer
+            sx={{
+                width: 240,
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                width: 240,
+                boxSizing: 'border-box',
+                marginTop: '64px',
+                paddingLeft: '16px',
+                overflowY: 'scroll', // Enable scrolling
+                '-ms-overflow-style': 'none', // Hide scrollbar, edge
+                '&::-webkit-scrollbar': {
+                    display: 'none', // Hide scrollbar, chrome
+                },
+                scrollbarWidth: 'none', // Hide scrollbar, firefox
+                },
+            }}
+            variant="permanent"
+            anchor="left"
             >
-              NexusSurge
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              The most <b>competitive</b> way to study
-            </Typography>
-            <Stack
-              sx={{ pt: 0 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button sx={{ fontFamily: 'Rubik' }} href='/sets' variant="contained">Create Study Sets</Button>
-              <Button sx={{ fontFamily: 'Rubik' }} href='/about' variant="outlined">About</Button>
-            </Stack>
-            <h1></h1>
-            <h1></h1>
-            <Typography
-            sx = {{mt: 10}}
-              component="h4"
-              variant="h4"
-              align="center"
-              color="text.primary"
-              gutterBottom
-              fontFamily={'jetbrains mono'}
-            >
-              Features
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                Speed up studying with interactive, engaging activities
-            </Typography>
-            {/* 3 inline cards */}
-            <Grid container spacing={2} sx={{justifyContent: 'center'}}>
-                <Grid item xs={12} sm={4}>
-                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Study
-                            </Typography>
-                            <Typography>
-                                Find a multiplayer match and compete against friends or others online on study sets
-                            </Typography>
+                <Box sx={{ p: 2 }}>
+                    <Typography variant="h6" gutterBottom>
+                    Choose Sets
+                    </Typography>
+                </Box>
+                <Divider />
+                <Stack spacing={2} sx={{ p: 2 }}>
+                    {test_user_sets_result.map((set) => (
+                    <Card key={set.id}>
+                        <CardContent>
+                        <Typography variant="h6" component="div">
+                            {set.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {set.description}
+                        </Typography>
                         </CardContent>
                         <CardActions>
-                            <Button href="/study" size="small">Find a Match</Button>
+                        <Button size="small">Study</Button>
                         </CardActions>
                     </Card>
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                        <CardContent sx={{ flexGrow: 1 }}>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Create Study Sets
-                            </Typography>
-                            <Typography>
-                                Create new study sets to share with friends and compete with up to date information
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button href="/sets" size="small">View</Button>
-                        </CardActions>
-                    </Card>
-                </Grid>
-            </Grid>
-          </Container>
-          {/* jank spacing thing */}
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-          <h3></h3>
-        </Box>
-        </div>
+                    ))}
+                </Stack>
+                </Drawer>
+                <Box sx={{ flexGrow: 1 }}>
+                {/* Main content */}
+                </Box>
+            </Box>
+            </Box>
         </>
     )
 }
