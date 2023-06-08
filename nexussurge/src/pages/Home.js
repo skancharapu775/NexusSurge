64e420830fa570b7516e2094d690128e9ee1c2a4
Alexander Popescu <alexanderpopescu5381@gmail.com>
2023-06-08 10:31:26 -0400

    finished homepage structure

--- nexussurge/src/pages/Home.js
+++ nexussurge/src/pages/Home.js
@@ -110,32 +110,40 @@ export function Home() {
 
   return (
     <>
-        <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1 }}>
         <Header />
         <Box sx={{ display: 'flex' }}>
             <SideDrawer sets={test_user_sets_result} />
-            <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', p: 25 }}>
-            <Grid container spacing={2} sx={{ width: '100%', justifyContent: 'center' }}>
-            {card_data_example.map((card, index) => (
-                <Grid item xs={3} key={index}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, p: 15, paddingRight: 10 }}>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 3 }}>
+                </Box>
+                <Grid container spacing={2} sx={{ width: '100%', justifyContent: 'center' }}>
+                {card_data_example.map((card, index) => (
+                    <Grid item xs={3} key={index}>
                     <a href={card.link} className='card-link'>
                         <Card className='center-card' sx={{ minHeight: '200px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out', '&:hover': { transform: 'translateY(-5px)', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)' } }} onClick={() => window.location.href=card.link}>
-                            <CardContent className='card-content'>
-                                <Typography variant="h5" component="h2" d>
-                                {card.title}
-                                </Typography>
-                                <Typography color="textSecondary">
-                                {card.description}
-                                </Typography>
-                            </CardContent>
+                        <CardContent className='card-content'>
+                            <Typography variant="h5" component="h2" d>
+                            {card.title}
+                            </Typography>
+                            <Typography color="textSecondary">
+                            {card.description}
+                            </Typography>
+                        </CardContent>
                         </Card>
                     </a>
-                </Grid>
+                    </Grid>
                 ))}
-            </Grid>
+                </Grid>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, paddingRight: '6vw', alignItems: 'center', justifyContent: 'center'}}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                    <Button variant="contained" color="primary" size="large" sx={{ m: 3, fontSize: 20, padding: '16px' }}>Create Party</Button>
+                    <Button variant="contained" color="secondary" size="large" sx={{ m: 3, fontSize: 20, padding: '16px' }}>Custom Settings</Button>
+                </Box>
             </Box>
         </Box>
-        </Box>
+      </Box>
     </>
   );
 }
