e1fea2ef1b7b35e2b0be1716533df32c6ace3529
Alexander Popescu <alexanderpopescu5381@gmail.com>
2023-06-10 13:08:28 -0400

    added scrolling for study sets and card overflow on study page

--- nexussurge/src/pages/Study.js
+++ nexussurge/src/pages/Study.js
@@ -39,6 +39,69 @@ export function Study() {
         creator: "Test User 5",
         num_cards: 10,
         id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
+    },
+    {
+        title: "Test Set 5 - Personal",
+        description: "This is a test set",
+        creator: "Test User 5",
+        num_cards: 10,
+        id: 5
     }
   ]
 
@@ -77,14 +140,21 @@ export function Study() {
         creator: "Test User 5",
         num_problems: 10,
         id: 5
-    }
+    },
+    {
+      title: "WS 6",
+      description: "This is a test WS",
+      creator: "Test User 6",
+      num_problems: 10,
+      id: 6
+  }
   ]
 
   return (
     <>
       <Header />
       <Box sx={{ display: 'flex', flexGrow: 1, height: '100%', marginTop: '8vh', fontFamily: 'Rubik, sans-serif' }}>
-      <Box sx={{ width: '50%', borderRight: 1, borderColor: 'divider', height: '100vh', fontFamily: 'Rubik, sans-serif', marginTop:'25px'}}>
+        <Box sx={{ width: '50%', borderRight: 1, borderColor: 'divider', height: '100vh', fontFamily: 'Rubik, sans-serif', marginTop:'25px'}}>
           <Typography variant="h3" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 0, textAlign: 'center' }}>
             Study Sets
           </Typography>
@@ -98,6 +168,7 @@ export function Study() {
             <Typography variant="h5" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 2, textAlign: 'center' }}>
               Your Sets
             </Typography>
+            <Box sx={{ overflowY: 'scroll', maxHeight: '60vh' }}>
             <Grid container spacing={2}>
               {studySets.map((set) => (
                 <Grid sx = {{marginBottom: '2rem'}} item xs={12} sm={6} md={4} key={set.name}>
@@ -125,53 +196,56 @@ export function Study() {
                 </Grid>
               ))}
             </Grid>
+            </Box>
           </Box>
         </Box>
         <Box sx={{ width: '50%', height: '100%', fontFamily: 'Rubik, sans-serif', marginTop:'25px'}}>
-        <Typography variant="h3" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 0, textAlign: 'center' }}>
-          Worksheets
-        </Typography>
-        <Box sx={{ p: 2 }}>
-          <TextField
-            label="Search"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-          <Typography variant="h5" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 2, textAlign: 'center' }}>
-            Your Worksheets
+          <Typography variant="h3" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 0, textAlign: 'center' }}>
+            Worksheets
           </Typography>
-          <Grid container spacing={2}>
-            {worksheets.map((worksheet) => (
-              <Grid sx={{ marginBottom: '2rem' }} item xs={12} key={worksheet.id}>
-                <Box
-                  sx={{
-                    p: 2,
-                    border: 1,
-                    borderColor: 'divider',
-                    borderRadius: 1,
-                    height: '100%',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    fontFamily: 'Rubik',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Box sx={{ flexGrow: 1 }}>
-                    <Typography variant="subtitle1" gutterBottom sx={{ fontFamily: 'Rubik', textAlign: 'left' }}>
-                      {worksheet.title}
-                    </Typography>
-                    <Typography variant="body2" gutterBottom sx={{ fontFamily: 'Rubik', textAlign: 'left' }}>
-                      {worksheet.num_problems} problems
-                    </Typography>
-                  </Box>
-                  <Button variant="outlined" sx={{ marginLeft: 'auto', '&:hover': { backgroundColor: 'primary.main', color: 'white' } }}>Download</Button>
-                </Box>
+          <Box sx={{ p: 2 }}>
+            <TextField
+              label="Search"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <Typography variant="h5" sx={{ p: 2, fontFamily: 'Rubik', paddingBottom: 2, textAlign: 'center' }}>
+              Your Worksheets
+            </Typography>
+            <Box sx={{ overflowY: 'scroll', maxHeight: '60vh' }}>
+              <Grid container spacing={2}>
+                {worksheets.map((worksheet) => (
+                  <Grid sx={{ marginBottom: '2rem' }} item xs={12} key={worksheet.id}>
+                    <Box
+                      sx={{
+                        p: 2,
+                        border: 1,
+                        borderColor: 'divider',
+                        borderRadius: 1,
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        fontFamily: 'Rubik',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Box sx={{ flexGrow: 1 }}>
+                        <Typography variant="subtitle1" gutterBottom sx={{ fontFamily: 'Rubik', textAlign: 'left' }}>
+                          {worksheet.title}
+                        </Typography>
+                        <Typography variant="body2" gutterBottom sx={{ fontFamily: 'Rubik', textAlign: 'left' }}>
+                          {worksheet.num_problems} problems
+                        </Typography>
+                      </Box>
+                      <Button variant="outlined" sx={{ marginLeft: 'auto', '&:hover': { backgroundColor: 'primary.main', color: 'white' } }}>Download</Button>
+                    </Box>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
+            </Box>
+          </Box>
         </Box>
-      </Box>
       </Box>
     </>
   );
